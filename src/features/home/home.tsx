16e64508a6a9e5cf740584026443904b155b0fe1
sumeet chandel <sumeetchandel321@gmail.com>
2024-12-07 03:02:49 +0530

    Card complete

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -1,6 +1,7 @@
 import Footer from "../../components/footer/footer";
 import Hero from "../../components/hero/hero";
 import Navbar from "../../components/navbar/navbar";
+import PopularRecipes from "../popular-recipes/popular-receipes";
 import CollaborateSection from "./components/collaborate-with-us";
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div>
       <Navbar />
       <Hero />
+      <PopularRecipes />
       <CollaborateSection />
       <Footer />
     </div>
---
+++ src/features/popular-recipes/popular-receipes.tsx
@@ -0,0 +1,54 @@
+import Card from "./components/card";
+import "./popular-recipes.scss";
+import data from "./data.json";
+const PopularRecipes = () => {
+  return (
+    <>
+      <div className="bg-slate-50 py-16">
+        <div className="px-10 py-5 flex justify-between items-center ">
+          <div className="info-section-recipes relative z-0">
+            <h1 className="font-dinBold text-[35px] m-0">POPULAR</h1>
+            <h1 className="font-dinBold text-[62px] m-0">RECIPES</h1>
+            <button className="font-dinBold text-[18px] py-1 px-3 text-black bg-primary">
+              SEE ALL
+            </button>
+          </div>
+          <div>
+            <h1 className="font-dinBold text-[35px] m-0">
+              DO YOU WANT TO SHARE YOUR OWN RECIPE?
+            </h1>
+            <div>
+              <button className="font-dinBold py-1 px-3 text-[18px] text-black bg-primary">
+                SEND IT NOW
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="px-10 my-16 grid grid-cols-4 gap-5">
+          {data?.result &&
+            data?.result.map(
+              ({ title, description, chefChoice, servings, id, img }) => {
+                {
+                  {
+                    console.log(data);
+                  }
+                }
+                return (
+                  <Card
+                    img={img}
+                    servings={servings}
+                    key={id}
+                    chefChoice={chefChoice}
+                    title={title}
+                    description={description}
+                  />
+                );
+              }
+            )}
+        </div>
+      </div>
+    </>
+  );
+};
+export default PopularRecipes;
